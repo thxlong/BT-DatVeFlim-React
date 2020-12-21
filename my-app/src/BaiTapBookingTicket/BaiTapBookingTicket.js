ae53f588b06c00a80365e1e84351f230f86d6586
Long Nguyen - PC <ntl.284@gmail.com>
2020-12-21 20:54:08 +0700

    layout

--- my-app/src/BaiTapBookingTicket/BaiTapBookingTicket.js
+++ my-app/src/BaiTapBookingTicket/BaiTapBookingTicket.js
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import HangGhe from "./HangGhe";
 import ThongTinDatGhe from "./ThongTinDatGhe";
 import "./BaiTapBookingTicket.css";
+import danhSachGheData from "../Data/danhSachGhe.json";
 
 export default class BaiTapBookingTicket extends Component {
+  renderHangGhe = () => {
+    return danhSachGheData.map((hangGhe, index) => {
+      return (
+        <Fragment key={index}>
+          <HangGhe hangGhe={hangGhe} soHangGhe={index} />
+        </Fragment>
+      );
+    });
+  };
+
   render() {
     return (
       <div
@@ -14,6 +25,7 @@ export default class BaiTapBookingTicket extends Component {
           height: "100%",
           backgroundImage: "url(./img/bgmovie.jpg)",
           backgroundSize: "100%",
+          // backgroundRepeat: "no-repeat",
         }}
       >
         <div
@@ -26,17 +38,24 @@ export default class BaiTapBookingTicket extends Component {
         >
           <div className="container-fluid">
             <div className="row">
-              <div className="col-8 display-4 text-warning text-center">
-                <HangGhe />
-                <div className="mt-5" style={{ fontSize: "25px" }}>
+              <div className="col-8 text-center text-light">
+                <span className="display-4">Hàng ghế</span>
+                <div
+                  className="mt-5 text-warning "
+                  style={{ fontSize: "25px" }}
+                >
                   Màn hình
                 </div>
                 <div
-                  className="d-flex flex-row mt-1"
-                  style={{ justifyContent: "center" }}
+                  className="d-flex mt-1"
+                  style={{
+                    justifyContent: "center",
+                    flexDirection: "row",
+                  }}
                 >
                   <div className="screen"></div>
                 </div>
+                {this.renderHangGhe()}
               </div>
 
               <div className="col-4 text-light">
--- my-app/src/BaiTapBookingTicket/HangGhe.js
+++ my-app/src/BaiTapBookingTicket/HangGhe.js
@@ -1,10 +1,52 @@
 import React, { Component } from "react";
 
 export default class HangGhe extends Component {
+  renderGhe = () => {
+    return this.props.hangGhe.danhSachGhe.map((ghe, index) => {
+      let cssGheDaDat = "";
+      let disable = false;
+      if (ghe.daDat) {
+        cssGheDaDat = "gheDuocChon";
+        disable = true;
+      }
+
+      return (
+        <button key={index} className={`ghe ${cssGheDaDat}`} disabled={disable}>
+          {ghe.soGhe}
+        </button>
+      );
+    });
+  };
+
+  renderSoHang = () => {
+    return this.props.hangGhe.danhSachGhe.map((hang, index) => {
+      return <button className="rowNumber"> {hang.soGhe}</button>;
+    });
+  };
+
+  renderHangGhe = () => {
+    if (this.props.soHangGhe === 0) {
+      return (
+        <div className="ml-3">
+          {this.props.hangGhe.hang}
+          {this.renderSoHang()}
+        </div>
+      );
+    } else
+      return (
+        <div>
+          {this.props.hangGhe.hang} {this.renderGhe()}
+        </div>
+      );
+  };
+
   render() {
     return (
-      <div>
-        <h1>HÀNG GHẾ</h1>
+      <div
+        className="text-left mt-2"
+        style={{ fontSize: "25px", marginLeft: "120px" }}
+      >
+        {this.renderHangGhe()}
       </div>
     );
   }
